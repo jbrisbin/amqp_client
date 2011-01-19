c8f08c20652e4a9f4250bc0f6d97ddcc5c7f6e0b
J. Brisbin <jon@jbrisbin.com>
2011-01-19 11:28:52 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+= Rebar-friendly fork of Erlang AMQP client
+
+This is a fork of the [official RabbitMQ/AMQP Erlang client](https://github.com/rabbitmq/rabbitmq-erlang-client). 
+
+It's meant to be included in your rebar projects in your rebar.config file:
+
+		{deps, [
+			{rabbit_common, ".*", {git, "git://github.com/jbrisbin/rabbit_common.git", "HEAD"}},
+			{amqp_client, ".*", {git, "git://github.com/jbrisbin/amqp_client.git", "HEAD"}}
+		]}.
+
+This is simply a re-packaging of the AMQP client, which is licensed under the MPL:
+
+		This package, the RabbitMQ server is licensed under the MPL. For the
+		MPL, please see LICENSE-MPL-RabbitMQ.
+
+		If you have any questions regarding licensing, please contact us at
+		info@rabbitmq.com.
---
+++ src/amqp_direct_connection.erl
@@ -0,0 +1,91 @@
+%% The contents of this file are subject to the Mozilla Public License
+%% Version 1.1 (the "License"); you may not use this file except in
+%% compliance with the License. You may obtain a copy of the License at
+%% http://www.mozilla.org/MPL/
+%%
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+%% License for the specific language governing rights and limitations
+%% under the License.
+%%
+%% The Original Code is RabbitMQ.
+%%
+%% The Initial Developer of the Original Code is VMware, Inc.
+%% Copyright (c) 2007-2010 VMware, Inc.  All rights reserved.
+%%
+
+%% @private
+-module(amqp_direct_connection).
+
+-include("amqp_client.hrl").
+
+-behaviour(amqp_gen_connection).
+
+-export([init/1, terminate/2, connect/4, do/2, open_channel_args/1, i/2,
+         info_keys/0, handle_message/2, closing/3, channels_terminated/1]).
+
+-record(state, {user,
+                vhost,
+                collector,
+                closing_reason %% undefined | Reason
+               }).
+
+-define(INFO_KEYS, [type]).
+
+%%---------------------------------------------------------------------------
+
+init([]) ->
+    {ok, #state{}}.
+
+open_channel_args(#state{user = User, vhost = VHost, collector = Collector}) ->
+    [User, VHost, Collector].
+
+do(_Method, _State) ->
+    ok.
+
+handle_message(Msg, State) ->
+    {stop, {unexpected_msg, Msg}, State}.
+
+closing(_ChannelCloseType, Reason, State) ->
+    {ok, State#state{closing_reason = Reason}}.
+
+channels_terminated(State = #state{closing_reason = Reason,
+                                   collector = Collector}) ->
+    rabbit_queue_collector:delete_all(Collector),
+    {stop, Reason, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+i(type, _State) -> direct;
+i(Item, _State) -> throw({bad_argument, Item}).
+
+info_keys() ->
+    ?INFO_KEYS.
+
+connect(AmqpParams, SIF, _ChMgr, State) ->
+    try do_connect(AmqpParams, SIF, State) of
+        Return -> Return
+    catch _:Reason -> {error, Reason}
+    end.
+
+do_connect(#amqp_params{username = Username, password = Pass,
+                        virtual_host = VHost},
+           SIF, State) ->
+    case lists:keymember(rabbit, 1, application:which_applications()) of
+        true  -> ok;
+        false -> exit(broker_not_found_in_vm)
+    end,
+    User = try rabbit_access_control:user_pass_login(Username, Pass) of
+               User1 -> User1
+           catch exit:#amqp_error{name = access_refused} -> exit(auth_failure)
+           end,
+    try rabbit_access_control:check_vhost_access(User, VHost) of
+        _ -> ok
+    catch exit:#amqp_error{name = access_refused} -> exit(access_refused)
+    end,
+    {ok, Collector} = SIF(),
+    {ok, {rabbit_reader:server_properties(), 0,
+          State#state{user = User,
+                      vhost = VHost,
+                      collector = Collector}}}.
