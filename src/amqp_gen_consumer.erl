7195c305bf039fa940c71ea9f3beb7cfbf4a30bd
Vladimir Baranov <vladimir.b.n.b@gmail.com>
2012-07-26 07:08:29 +0300

    fix return value in  amqp_gen_consumer

--- src/amqp_gen_consumer.erl
+++ src/amqp_gen_consumer.erl
@@ -204,8 +204,10 @@ handle_call({consumer_call, Msg}, From,
             {noreply, State#state{module_state = NewMState}};
         {reply, Reply, NewMState} ->
             {reply, Reply, State#state{module_state = NewMState}};
+        {error, {consumer_died, Reason}, NewMState} ->
+            {stop, Reason, State#state{module_state = NewMState}};
         {error, Reason, NewMState} ->
-            {stop, {error, Reason}, {error, Reason},
+            {stop, {error, Reason},
              State#state{module_state = NewMState}}
     end;
 handle_call({consumer_call, Method, Args}, _From,
